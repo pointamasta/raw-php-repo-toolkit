bbbe6f34a256a5fb47fde00aec43434c231a4cf0
Antoine la Mache <antoine.la-mache@epitech.eu>
2020-10-02 15:27:46 +0200

    coucpo

--- print_file.php
+++ print_file.php
@@ -1,3 +1,4 @@
 <?php
-$dir    = '/tmp'; $files1 = scandir($dir); $files2 = scandir($dir, 1);  print_r($files1); print_r($files2);
+$dir    = '.'; $files1 = scandir($dir); $files2 = scandir($dir, 1);  print_r($files1); print_r($files2);
+
 ?>
